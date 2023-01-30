0e18e9663ec89d958267a9ad4a4eb52f8b3966ce
root <tidus8404@gmail.com>
2023-01-30 09:56:41 -0700

    new file:   initial_setup.sh

---
+++ initial_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Choose VM
+echo -e "What VM are you in today?\nAnswer with number\n100. General\n150. Kali\n152. Caldera\n201. SIEM\n202. Logger\n203. Cuckoo\n240. Training"
+read vmnum
+
+#Set static IP
+echo "Setting static IPs for VM ID.$vmnum"
+cp /etc/netplan/01-network-manager-all.yaml /etc/netplan/01-network-manager-all.yaml.bak
+echo -e "# Let NetworkManager manage all devices on this system\nnetwork:\n  version: 2\n  ethernets:\n    ens18:\n      dhcp4: no\n      addresses:\n        - 10.100.10.$vmnum/24\n      gateway4: 10.100.10.1\n      nameservers:\n          addresses: [10.100.1.12, 10.100.1.13, 10.100.1.1]\n    ens19:\n      dhcp4: no\n      addresses:\n        - 10.100.11.$vmnum/24\n      gateway4: 10.100.11.1\n      nameservers:\n          addresses: [10.100.1.12, 10.100.1.13, 10.100.1.1]" > /etc/netplan/01-network-manager-all.yaml
+netplan apply
