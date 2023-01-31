c3b2f44703b725d8dfc367a20e56ab8bf1c428b0
root <tidus8404@gmail.com>
2023-01-31 12:50:43 -0700

    Created zeek container 	modified:   logger_setup.sh

--- logger_setup.sh
+++ logger_setup.sh
@@ -12,7 +12,7 @@ echo -e "Installing make"
 apt install make -y
 
 
-#Pull Containers
+#Pull and Create Containers
 echo -e "Pulling and Creating Suricata container"
 docker pull jasonish/suricata
 docker run --name suricata --rm -it -d --net=host --cap-add=net_admin --cap-add=net_raw --cap-add=sys_nice jasonish/suricata:latest -i ens18 -i ens19
@@ -22,9 +22,16 @@ cd /home/yuna/zeek
 git clone https://github.com/zeek/zeek-docker.git
 cd /home/yuna/zeek/zeek-docker
 make build-stamp_4.2.0
+docker run --name zeek -it -d  --net=host broplatform/bro:latest -i ens18 -i ens19
 echo -e "Pulling and Creating ntop container"
 mkdir -p /home/yuna/ntop
 cd /home/yuna/ntop
 docker pull ntop/ntopng
 docker run --name ntopng -it -d -p 3000:3000 --net=host ntop/ntopng:latest -i ens18 -i ens19
+
+#Ensure static ips
+echo "Rerunning netplan to ensure static IPs that changed go to intended static IPs"
+netplan apply
+
+
 echo -e "Created ntopng container\nUse cmd 'docker exec -it ntopng /bin/bash' to access container"
