f81dc3b1282b6a40f25ed3368cdd163c4b8ee77b
root <tidus8404@gmail.com>
2023-01-30 17:41:39 -0700

    modified:   logger_setup.sh

--- logger_setup.sh
+++ logger_setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+##### Setup Steps in echos#####
+
 #Set crontab
+echo -e "Setting up crontab"
 echo -e "0 2 * * 5 apt update\n0 3 * * 5 apt upgrade -y\n0 4 * * 5 shutdown -r" > mycron
 crontab mycron
+
+#Pull Containers
+echo -e "Pulling Suricata container"
+docker pull jasonish/suricata
+echo -e "Pulling Zeek container"
+
+echo -e "Pulling ntop container"
+
