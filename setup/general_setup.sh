f9c9abc0207dc573eac9ff810c8721bc2e6fa060
root <tidus8404@gmail.com>
2023-02-02 13:04:17 -0700

    general vm updates 	renamed:    cron/general_cron2.sh -> cron/general_ssh.sh 	renamed:    cron/general_cron.sh -> cron/general_update.sh 	modified:   setup/general_setup.sh

--- setup/general_setup.sh
+++ setup/general_setup.sh
@@ -2,6 +2,6 @@
 
 #Set crontab
 echo -e "Setting up crontab"
-echo -e "0 0 * * 0 bash /home/tidus/Documents/git/cron/general_cron.sh" > mycron
-echo -e "@reboot bash /home/tidus/Documents/git/cron/general_cron2.sh" >> mycron
+echo -e "0 0 * * 0 bash /home/tidus/Documents/git/cron/general_update.sh" > mycron
+echo -e "@reboot bash /home/tidus/Documents/git/cron/general_ssh.sh" >> mycron
 crontab mycron
