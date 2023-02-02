68d91d999aaec6370037f9df77e8ac62195be3a4
root <tidus8404@gmail.com>
2023-02-02 15:07:10 -0700

    Updated docker commands 	deleted:    setup/.siem_setup.sh.swp 	modified:   setup/siem_setup.sh

--- setup/siem_setup.sh
+++ setup/siem_setup.sh
@@ -4,9 +4,14 @@
 
 # Pulling Containers
 echo -e "Pulling Splunk container"
-docker
+docker pull splunk/splunk:latest
+echo -e "Starting Splunk Container"
+docker run --name splunk -it -d --net=host -p 8000:8000 -e SPLUNK_START_ARGS='--accept-license' -e SPLUNK_PASSWORD='splunkadmin' splunk/splunk:latest -i ens18
 
 echo -e "Pulling ELK Container"
+docker pull sebp/elk
+echo -e "Starting ELK Container"
+docker run --name elk -it -d --net=host -p 5601:5601 -p 9200:9200 -p 5044:5044 sebp/elk:latest -i ens18
 
 #Set crontab
 echo -e "Setting up crontab"
