6854fc247dae92bd34a9514c8ab2cc5a84827d17
root <tidus8404@gmail.com>
2023-02-03 08:25:36 -0700

    added restart capabilities 	modified:   setup/siem_setup.sh

--- setup/siem_setup.sh
+++ setup/siem_setup.sh
@@ -6,12 +6,12 @@
 echo -e "Pulling Splunk container"
 docker pull splunk/splunk:latest
 echo -e "Starting Splunk Container"
-docker run --name splunk -it -d --net=host -p 8000:8000 -e SPLUNK_START_ARGS='--accept-license' -e SPLUNK_PASSWORD='splunkadmin' splunk/splunk:latest -i ens18
+docker run --name splunk -it -d --restart unless-stopped --net=host -p 8000:8000 -e SPLUNK_START_ARGS='--accept-license' -e SPLUNK_PASSWORD='splunkadmin' splunk/splunk:latest -i ens18
 
 echo -e "Pulling ELK Container"
 docker pull sebp/elk
 echo -e "Starting ELK Container"
-docker run --name elk -it -d --net=host -p 5601:5601 -p 9200:9200 -p 5044:5044 sebp/elk:latest -i ens18
+docker run --name elk -it -d -d --restart unless-stopped --net=host -p 5601:5601 -p 9200:9200 -p 5044:5044 sebp/elk:latest -i ens18
 
 #Set crontab
 echo -e "Setting up crontab"
