4ca03e04391c8ea926d0fbd05b62bcac0bf6566e
root <tidus8404@gmail.com>
2023-03-15 14:33:34 -0600

    New Kali ISO 	modified:   prox/initial_vms.sh

--- prox/initial_vms.sh
+++ prox/initial_vms.sh
@@ -4,7 +4,7 @@ echo "Checking if initial VMs are already created"
 qm list
 
 echo -e "These are the current VMs created.\n\nCreating the remainder VMs now"
-qm create 150 --name Kali --cdrom local:iso/kali-linux-2022.4-installer-amd64.iso --scsi0 VM-Main:64 --ostype l26 --memory 8192 --sockets 1 --cores 4 --net0 virtio,bridge=vmbr0,firewall=0 --net1 virtio,bridge=vmbr1,firewall=0
+qm create 150 --name Kali --cdrom local:iso/kali-linux-2023.1-installer-amd64.iso --scsi0 VM-Main:64 --ostype l26 --memory 8192 --sockets 1 --cores 4 --net0 virtio,bridge=vmbr0,firewall=0 --net1 virtio,bridge=vmbr1,firewall=0
 
 echo -e "Setting static IPs for the VMs"
 
