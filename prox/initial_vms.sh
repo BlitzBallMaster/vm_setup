aa5d3880b65b9fedc6cf5467f641cc2ecc0d7e18
root <tidus8404@gmail.com>
2023-03-16 09:20:46 -0600

    Realized that host needs to be on for ansible to successfully ping 	modified:   prox/hosts 	modified:   prox/initial_vms.sh

--- prox/initial_vms.sh
+++ prox/initial_vms.sh
@@ -3,7 +3,7 @@
 echo "Checking if initial VMs are already created"
 qm list
 
-echo -e "These are the current VMs created.\n\nCreating the remainder VMs now"
+echo -e "These are the current VMs created.\n\nCreating the remaining VMs and setting MACs"
 #qm create 100 --name General --cdrom local:iso/ubuntu-22.04.1-desktop-amd64.iso --scsi0 VM-Main:64 --ostype l26 --memory 8192 --sockets 1 --cores 4 --net0 virtio=CE:A6:1A:16:FD:10,bridge=vmbr0,firewall=0 --net1 virtio=76:D3:BA:30:24:10,bridge=vmbr1,firewall=0&
 qm create 150 --name Kali --cdrom local:iso/kali-linux-2023.1-installer-amd64.iso --scsi0 VM-Main:64 --ostype l26 --memory 8192 --sockets 1 --cores 4 --net0 virtio=CE:A6:1A:16:FD:15,bridge=vmbr0,firewall=0 --net1 virtio=76:D3:BA:30:24:15,bridge=vmbr1,firewall=0&
 #qm create 151 --name Commando --cdrom local:iso/Windows_10_x64.iso --sata0 VM-Main:64 --ostype win10 --memory 8192 --sockets 1 --cores 4 --net0 virtio=CE:A6:1A:16:FD:1A,bridge=vmbr0,firewall=0 --net1 virtio=76:D3:BA:30:24:1A,bridge=vmbr1,firewall=0&
@@ -14,13 +14,18 @@ qm create 150 --name Kali --cdrom local:iso/kali-linux-2023.1-installer-amd64.is
 #qm create 204 --name ThreatPursuit --cdrom local:iso/Windows_10_x64.iso --sata0 VM-Main:64 --ostype win10 --memory 8192 --sockets 1 --cores 4 --net0 virtio=CE:A6:1A:16:FD:24,bridge=vmbr0,firewall=0 --net1 virtio=76:D3:BA:30:24:24,bridge=vmbr1,firewall=0&
 wait
 
-echo -e "Setting MACs"
-qm set 150 --ipconfig1 ip=10.100.11.150/24,gw=10.100.11.1
-#eth0 CE:A6:1A:16:FD:15
-#eth1 76:D3:BA:30:24:15
-
 echo -e "Finished creating the following VMs and setting Macs"
 qm list | awk '{print $1,$2}'
+
+echo -e "Starting the VMs"
+qm start 100&
+qm start 150&
+qm start 151&
+qm start 152&
+qm start 201&
+qm start 202&
+qm start 203&
+qm start 204&
 
 echo -e "Copying Ansible hosts file over"
 cp ./prox/hosts /etc/ansible/hosts
